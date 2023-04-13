3eeab7485267ae9669ea01a5f21f5dd6669ea627
David Nagy <david.nagy@quinto.io>
2023-04-13 18:15:56 +0200

    0.30.2 - hotfix export

---
+++ src/db/index.ts
@@ -0,0 +1,5 @@
+import Translation from './entity/Translation.js';
+import Language from './entity/Language.js';
+import LangDb from './LangDb.js';
+
+export { Translation, Language, LangDb };
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import LangClient from './client/LangClient.js';
 import LangModule from './LangModule.js';
 
 export * from './lib/index.js';
+export * from './db/index.js';
 
 export { GLang, LangModule, LangClient };
 export default LangModule;
